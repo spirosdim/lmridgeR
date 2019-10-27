81436feaeb807b19d95a014821382ad14111efdb
spirosdim <spirosdmtr@gmail.com>
2019-10-27 21:29:39 +0100

    add vizualization of the dalay

--- bonusLab/R/visualize_airport_delays.R
+++ bonusLab/R/visualize_airport_delays.R
@@ -3,15 +3,28 @@
 #' @description A function that Visualize Airport Delays from NY Airport
 #' @import dplyr
 #' @import ggplot2
+#' @import 
+#' @import 
 #' 
 visualize_airport_delays <- function(){
-  fli <- nycflights13::flights
-  air <- nycflights13::airports
-  delay <-dplyr::select(fli,dep_delay,origin)
-  mean_delay <- delay %>% group_by(origin) %>% summarise(mean_size = mean(dep_delay,na.rm = TRUE))
-  exact_pool <- dplyr::filter(air,faa %in% mean_delay$origin)
-  exact_la_lo <- dplyr::select(exact_pool,faa,lat,lon)
-  mean_delay <- dplyr::bind_cols(mean_delay,exact_la_lo)
-  plot_dat <-data.frame(mean_delay)
-  ggplot(plot_dat,aes(x=lon,y=lat))+geom_point(size=2) + geom_text(aes(label=origin),hjust=0, vjust=2)
+  fl_delays <- select(nycflights13::flights, arr_delay, dest)
+  air_long_lat <- select(nycflights13::airports, faa, lat, lon)
+  
+  data <- fl_delays %>%
+    group_by(dest) %>%
+    summarise(mean_delay=mean(arr_delay, na.rm=TRUE)) %>%
+    left_join(air_long_lat, by=c("dest"="faa")) %>%
+    filter(!is.na(mean_delay), !is.na(lat),lat<50,lat>24) #remove nans and some outliers 
+  
+  usa <- map_data("usa") # we already did this, but we can do it again
+  gg <- ggplot(data = usa,  mapping =aes(x=long, y = lat, group = group))+
+    geom_polygon() +
+    coord_fixed(1.3)+
+    geom_point(data, mapping = aes(x=lon ,y=lat, group = mean_delay ,color=mean_delay))+
+    scale_colour_gradient(high = "red",low="yellow")+
+    ggtitle("Mean arrival delay to airports")+
+    xlab("Latitude")+
+    ylab("Longitude")
+  
+  return(gg)
 }
